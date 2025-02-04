f8df82ed5c63ad2e6957b94fbb293f9fc6457c10
nischup <ahsabbir23@gmail.com>
2025-02-04 14:29:03 +0600

    package name updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.drive-lingua.apps',
-  appName: 'Drive-Lingua',
+  appId: 'de.drive.lingua.apps',
+  appName: 'Drive Lingua',
   webDir: 'www'
 };
 
